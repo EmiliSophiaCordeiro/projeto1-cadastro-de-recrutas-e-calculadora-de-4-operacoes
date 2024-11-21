72712f6159c09b90b55d7d9f9e30dc77747b1181
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-21 09:33:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// cadastro de recrutas
+// Cadastro de recrutas
 
-let sobrenome = 'Digite seu sobrenome:'
-let curso = 'Digite seu curso da base técnica:'
-const anoNascimento = parseInt('Digite seu ano de nascimento:')
+let primeiroNome = prompt('Digite seu primeiro nome:');  // Solicita o primeiro nome
+let sobrenome = prompt('Digite seu sobrenome:');  // Solicita o sobrenome
+let curso = prompt('Digite seu curso da base técnica:');  // Solicita o curso
+const anoNascimento = parseInt(prompt('Digite seu ano de nascimento:'));  // Solicita o ano de nascimento e converte para número
 
-let idade = 2024 - anoNascimento
+let idade = 2024 - anoNascimento;  // Calcula a idade com base no ano de nascimento
 
 console.log(`
     Olá ${primeiroNome} ${sobrenome}
     Então você estuda ${curso} e tem ${idade} anos. Legal!
-`)
+`);
 
-//calculadora
-let num1 = parseInt('Digite um número:')
-let num2 = parseInt('Digite outro número:')
+// Calculadora
 
-let soma = num1 + num2
-let sub = num1 - num2
-let multi = num1 * num2
-let div = num1 / num2
+let num1 = parseInt(prompt('Digite um número:'));  // Solicita o primeiro número
+let num2 = parseInt(prompt('Digite outro número:'));  // Solicita o segundo número
+
+let soma = num1 + num2;  // Realiza a soma
+let sub = num1 - num2;  // Realiza a subtração
+let multi = num1 * num2;  // Realiza a multiplicação
+let div = num1 / num2;  // Realiza a divisão
 
 console.log(`
     Resultado:
@@ -26,4 +28,4 @@ console.log(`
     ${num1} - ${num2} = ${sub}
     ${num1} * ${num2} = ${multi}
     ${num1} / ${num2} = ${div}
-`)
+`);
